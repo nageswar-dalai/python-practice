b3850d499b8e6a7ef7bf2e4e8c9935f79c5a3ed6
Nageswar Dalai <nageswardalai40@gmail.com>
2026-07-04 18:23:29 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,22 @@
+#using arithmatic operator(+,-,*,/,%.//) getting output
+x=50
+y=30
+print(f"sum is={x+y}")    #using f string
+print(f"diff is={x-y}") 
+print(f"product is={x*y}") 
+print(f"div is={x/y}") 
+print(f"remainder is={x%y}") 
+print(f"flor div is={x//y}") 
+
+#define square and cube
+a=5
+print(a**2)
+print(a**3)
+
+#using comparision operator(>,<,==,!=)
+num1=20
+num2=50
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+print(num1!=num2)
