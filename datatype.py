32578a1d812cc2c2695773bfd6ea96e2a5a61f28
Nageswar Dalai <nageswardalai40@gmail.com>
2026-07-04 18:02:30 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,27 @@
+#create some varible and store int,float,str and bool type of values.And check the the value is store which type of data.
+a=10
+b=2.3
+c="nageswar"
+d=True
+print("a is:",type(a))
+print("b is:",type(b))
+print("c is:",type(c))
+print("d is:",type(d))
+
+# 5 fruits store in list
+fruits=["apple","mango","banana","graps"]
+print(fruits)
+print(type(fruits))
+
+# 3 colours are store in tuple
+colors=("red","green","black")
+print(colors)
+
+# name,age,city store in dict
+inform={"name":"nageswar","age":23,"city":"hyderbad"}
+print(inform)
+
+# print set store duplicate value
+nums={22,23,32,2,3,22,3}
+print(nums)
+print(type(nums))
