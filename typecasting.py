21bde885fb244a98117d5f6ba7866efe06decf49
Nageswar Dalai <nageswardalai40@gmail.com>
2026-07-04 18:50:11 +0530

    Create typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+#typecasting- 
+# float to int and add 50
+a=int("100")
+print(a+50)
+
+#int to float
+b=float(25)
+print(type(b))
+
+#float to int
+c=int(10.8)
+print(type(c))
+
+#int to str
+d="50"
+print(type(d))
