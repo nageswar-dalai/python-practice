84182c0fec137100c03342be4793e84a24bb6b91
Nageswar Dalai <nageswardalai40@gmail.com>
2026-07-04 17:19:38 +0530

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -7,3 +7,18 @@ print("Name:", name)
 print("Age:", age)
 print("City:", city)
 
+#print num=50 and age=23
+num=50
+age=23
+print(num)
+print(age)
+
+#print city="Bhubaneswar"
+city="Bhubaneswar"
+print(city)
+
+# swap the value of a=10 and b=20 without using a temporary variable.
+a=10
+b=20
+print("a=",b)
+print("b=",a)
